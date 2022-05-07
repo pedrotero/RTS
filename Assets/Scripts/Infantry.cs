19461a1fe87241f5861f2608fe54aa6d50d2a6c1
pedrotero <poteroa@uninorte.edu.co>
2022-05-07 08:56:23 -0500

    Kumbalala

--- Assets/Scripts/Infantry.cs
+++ Assets/Scripts/Infantry.cs
@@ -13,6 +13,11 @@ public class Infantry : MonoBehaviour
     float MaxHealth;
     public Canvas cv;
     public HealthBar hb;
+    public bool team;
+    public Vector3 target;
+    public Transform tr;
+    private Collider[] nearby;
+    private bool Chasing;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,22 +26,57 @@ public class Infantry : MonoBehaviour
         cv.worldCamera = Camera.main;
         cam = Camera.main;
         agent = GetComponent<NavMeshAgent>();
+        tr = GetComponent<Transform>();
+        Chasing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetMouseButtonDown(0))
+        if (!Chasing)
         {
-            Health *= 0.66f;
-            hb.UpdateDMG(Health/MaxHealth);
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                agent.SetDestination(hit.point);
-            }
+            nearby = Physics.OverlapSphere(tr.position, 10, 8, QueryTriggerInteraction.Ignore);
+            target = tr.position; //cambiar por nexo
 
+            float closest = 11;
+            foreach (Collider hit in nearby)
+            {
+                Debug.Log("Hello World");
+
+
+                float dis = Vector3.Distance(hit.transform.position, tr.position);
+                if (dis < closest)
+                {
+                    closest = dis;
+                    target = hit.transform.position;
+                    Debug.Log("chasing: " + hit.name + " with Vector3 "+ target);
+                }
+            }
+                Chasing = true;
+                agent.SetDestination(target);
+            
         }
+        Debug.Log("remdist" + agent.remainingDistance);
+
+        //if (Input.GetMouseButtonDown(0))
+        //{ 
+        //    Health *= 0.66f;
+        //    hb.UpdateDMG(Health/MaxHealth);
+        //    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        //    if (Physics.Raycast(ray, out RaycastHit hit))
+        //    {
+        //        agent.SetDestination(hit.point);
+        //    }
+        //    if(agent.remainingDistance < agent.stoppingDistance)
+        //    {
+        //        //attack
+        //    }
+
+        //}
+
+
+
+
     }
 }
