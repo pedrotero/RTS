fc5d06537abc546798e41a0dbe3a9f424fd3a051
Luis E Avila <54752453+luisd159@users.noreply.github.com>
2022-05-20 10:48:21 -0500

    tank

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Tank : Soldier
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextAttack = 0;
+        FireRate = 2;
+        attackRadius = 4;
+        GetComponent<Collider>().enabled = false;
+        MaxHealth = 150;
+        Health = MaxHealth;
+        cv.worldCamera = Camera.main;
+        tr = GetComponent<Transform>();
+        rig = GetComponent<Rigidbody>();
+        Chasing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Chasing && agent)
+        {
+            float radius = 20;
+            nearby = Physics.OverlapSphere(tr.position, radius, 8);
+            nearby = nearby.Where(h => h.GetComponent<Soldier>().team != team).ToArray();
+            float closest = radius + 1;
+            foreach (Collider hit in nearby)
+            {
+
+                float dis = Vector3.Distance(hit.transform.position, tr.position);
+                if (dis <= closest)
+                {
+                    closest = dis;
+                    target = hit.GetComponent<Soldier>();
+                    agent.SetDestination(target.tr.position);
+                }
+                Chasing = true;
+
+            }
+            if (nearby.Length == 0)
+            {
+                Chasing = false;
+                agent.ResetPath();
+                //cambiar por nexo
+                target = nexoTarget;
+            }
+
+        }
+        if (Chasing && target && agent)
+        {
+            agent.SetDestination(target.tr.position);
+        }
+
+        if (target && agent)
+        {
+            Vector3 attackPoint = target.GetComponent<Collider>().ClosestPointOnBounds(tr.position);
+            float dist2Att = (attackPoint - tr.position).magnitude;
+            if (agent && target && dist2Att <= attackRadius && Time.time >= NextAttack)
+            {
+                //attack
+                Vector3 dir = (tr.position - target.tr.position).normalized * 5;
+
+                target.SendMessage("takeDamage", new Vector4(dir.x, dir.y, dir.z, 10));
+
+                NextAttack = Time.time + FireRate;
+                agent.ResetPath();
+                target = null;
+                agent.isStopped = true;
+                Chasing = false;
+                Vector3 vkb = dir; //en direccion contraria 
+                rig.velocity = vkb;
+                Invoke(nameof(restartAgent), vkb.magnitude * 0.2f);
+            }
+        }
+
+    }
+}
